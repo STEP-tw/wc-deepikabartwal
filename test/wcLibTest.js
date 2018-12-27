b9b89ee841623b0d4603af816ceeae8836f96a08
Deepika <44020585+deepikabartwal@users.noreply.github.com>
2018-12-27 10:14:56 +0530

    added test for single options mentioned

--- test/wcLibTest.js
+++ test/wcLibTest.js
@@ -1,10 +1,10 @@
 const assert = require('assert');
 const { wc } = require('../src/lib.js');
-
+const { NEWLINE, TAB, SPACE, EMPTY } = require('../src/constant.js');
 const fs = {};
 
 const dummyFiles = {
-  'oneLine.txt': 'this is one line\n'
+  'oneLine.txt': 'this is one line'
 };
 
 fs.readFileSync = function(path, encoding) {
@@ -18,9 +18,28 @@ fs.existsSync = function(path) {
   return true;
 };
 describe('wc', () => {
-  it('should return number of lines, words and bytes in a file for default command', () => {
-    let actual = wc('oneLine.txt', fs);
-    let expectedOutput = '\t1\t4\t17 oneLine.txt';
-    assert.equal(actual, expectedOutput);
+  describe('for default arguments', () => {
+    it('should return number of lines, words and bytes in a file for default command', () => {
+      let actual = wc(['oneLine.txt'], fs);
+      let expectedOutput = '\t0\t4\t16 oneLine.txt';
+      assert.equal(actual, expectedOutput);
+    });
+  });
+  describe('for option specified', () => {
+    it('should return number of lines in a file when -l is mentioned', () => {
+      let actual = wc(['-l', 'oneLine.txt'], fs);
+      let expectedOutput = '\t0 oneLine.txt';
+      assert.equal(actual, expectedOutput);
+    });
+    it('should return number of words in a file when -w is mentioned', () => {
+      let actual = wc(['-w', 'oneLine.txt'], fs);
+      let expectedOutput = '\t4 oneLine.txt';
+      assert.equal(actual, expectedOutput);
+    });
+    it('should return number of bytes in a file when -w is mentioned', () => {
+      let actual = wc(['-c', 'oneLine.txt'], fs);
+      let expectedOutput = '\t16 oneLine.txt';
+      assert.equal(actual, expectedOutput);
+    });
   });
 });
