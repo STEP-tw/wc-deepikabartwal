dbb0b349ac96988a77b5c6d0e8c4697823398446
Deepika <44020585+deepikabartwal@users.noreply.github.com>
2018-12-27 14:17:52 +0530

    modified parseArgs for multiple options
    modified parseArgs
    now it returns object of option array and files array.

--- src/parser.js
+++ src/parser.js
@@ -1,20 +1,25 @@
-const { defaultFormatter, singleOptionFormatter } = require('./formatter.js');
-
+const removeHyphen = function(args) {
+  return args.slice(1);
+};
 const beginsWithDash = function(arg) {
   return arg.startsWith('-');
 };
 
+const notBeginsWithDash = function(arg) {
+  return !arg.startsWith('-');
+};
 const parseArgs = function(args) {
-  const firstArg = args[0];
-  let option = 'all';
-  let formatter = defaultFormatter;
-  let fileName = args[0];
-  if (beginsWithDash(firstArg)) {
-    option = firstArg;
-    formatter = singleOptionFormatter;
-    fileName = args[1];
+  let options = args.filter(beginsWithDash);
+  let files = args.filter(notBeginsWithDash);
+  let optionList = options
+    .map(removeHyphen)
+    .join('')
+    .split('');
+
+  if (options.length == 0) {
+    return { files: files, options: ['l', 'w', 'c'] };
   }
-  return { option, formatter, fileName };
+  return { files: files, options: optionList };
 };
 module.exports = {
   parseArgs
