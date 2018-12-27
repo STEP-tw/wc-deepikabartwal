87fda4219420e2c2ebbe9f2f6f59c822d0a9bce2
Deepika <44020585+deepikabartwal@users.noreply.github.com>
2018-12-27 15:27:25 +0530

    modified wc for multiple files and getting total
    modified wc for multiple files
    implemented a function for getting total of counts of multiple files

--- src/lib.js
+++ src/lib.js
@@ -16,21 +16,50 @@ const countBytes = function(content) {
   return fetchBytes(content).length;
 };
 
+const temp = function(fs, options) {
+  return function(file) {
+    let content = fs.readFileSync(file, 'utf-8');
+    let countList = [''];
+    if (options.includes('l')) {
+      countList.push(countLines(content));
+    }
+    if (options.includes('w')) {
+      countList.push(countWords(content));
+    }
+    if (options.includes('c')) {
+      countList.push(countBytes(content));
+    }
+    countList.push(file);
+
+    return countList;
+  };
+};
+const joinOnTab = function(list) {
+  return list.join(TAB);
+};
+
+const joinByNewline = function(list) {
+  return list.join(NEWLINE);
+};
+
+const getTotalCount = function(list1, list2) {
+  let totalCountList = [''];
+  for (let index = 1; index < list1.length - 1; index++) {
+    totalCountList[index] = list1[index] + list2[index];
+  }
+  return totalCountList;
+};
+
 const wc = function(args, fs) {
   let { files, options } = parseArgs(args);
-  let content = fs.readFileSync(files[0], 'utf-8');
-  let countList = [];
-  if (options.includes('l')) {
-    countList.push(countLines(content));
+  let getCounts = temp(fs, options);
+  let countList = files.map(getCounts);
+  if (files.length > 1) {
+    let total = countList.reduce(getTotalCount);
+    total.push('total');
+    countList.push(total);
   }
-  if (options.includes('w')) {
-    countList.push(countWords(content));
-  }
-  if (options.includes('c')) {
-    countList.push(countBytes(content));
-  }
-  countList.push(files[0]);
-  return countList.join('\t');
+  return joinByNewline(countList.map(joinOnTab));
 };
 
 module.exports = {
